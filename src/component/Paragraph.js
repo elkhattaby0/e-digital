6b5a9e0e1d691256946c3743cdb8db1967d5d298
elkhattaby0 <eelkhattaby@gmail.com>
2025-04-29 18:18:41 +0100

    v1 header

--- src/component/Paragraph.js
+++ src/component/Paragraph.js
@@ -3,7 +3,7 @@ import React from "react"
 const Paragraph = ({ children, isCenter = false }) => {
     return (
         <p
-            className={` ${ isCenter ? "text-center" : "" } font-base text-md text-[#000000] leading-[24px] mb-4 dark:text-[#E5E5E5]`}
+            className={` ${ isCenter ? "text-center" : "" } font-base text-md text-[#000000] leading-relaxed mb-4 dark:text-[#E5E5E5]`}
         >
             { children }
         </p>
