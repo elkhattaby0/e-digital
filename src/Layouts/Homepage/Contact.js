111aa7e0d9427e5e8d0f641b2661077149a1de7e
elkhattaby0 <eelkhattaby@gmail.com>
2025-04-29 17:48:18 +0100

    v1 error 404

--- src/Layouts/Homepage/Contact.js
+++ src/Layouts/Homepage/Contact.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, LucideMail, LucideMapPin, LucidePhone } from "lucide-react";
 import HeaderTwo from "../../component/HeaderTwo";
 import Paragraph from "../../component/Paragraph";
 import emailjs from 'emailjs-com';
@@ -8,6 +8,11 @@ emailjs.init('4UGpHdCAb_fLz0-ub');  // ← your Public Key
 
 
 const Contact = () => {
+    const contactinfo = [
+        { "name": "Phone", "value": "+212 650 999 953", "href": "tel:+212650999953", "icon": <LucidePhone size="35px" className="text-[#14213D] dark:text-[#E5E5E5]"/> },
+        { "name": "Email", "value": "contact@edigital.com", "href": "mailto:contact@edigital.com", "icon": <LucideMail size="35px" className="text-[#14213D] dark:text-[#E5E5E5]"/> },
+        { "name": "Address", "value": "86000 Agadir, Morocco", "href": "Agadir, Morocco", "icon": <LucideMapPin size="35px" className="text-[#14213D] dark:text-[#E5E5E5]"/> }
+    ]
 
     const sendEmail = (e) => {
         e.preventDefault();
@@ -32,36 +37,55 @@ const Contact = () => {
     return (
         <section className={Style.container} >
             <div className={Style.section} >
-                <div className="w-1/2 max-md:w-full">
+                <div className="w-1/2 max-md:w-full max-md:mb-4">
                     <HeaderTwo>Let’s Work Together</HeaderTwo>
-                    <Paragraph>Have a question or project ? Send us a message. We reply fast.</Paragraph>
+                    <Paragraph>Ready to start your next project ? Contact us today for a free consultation.</Paragraph>
+                    <div className="flex flex-col gap-4">
+                        {
+                            contactinfo.map(n=> (
+                                <div className="flex items-center gap-4" key={n.name}>
+                                    {n.icon}
+                                    <div className="flex flex-col">
+                                        <label
+                                            className="text-[#14213D] dark:text-[#E5E5E5] font-semibold text-xl"
+                                        >{n.name}</label>
+                                        <a 
+                                            href={n.href}
+                                            className="text-[#14213D] dark:text-[#E5E5E5] font-base text-sm"
+                                        >{n.value}</a>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                        
+                    </div>
                 </div>
                 <div className="w-1/2 max-md:w-full">
                     <form onSubmit={sendEmail}>
                         <div className="w-full block gap-2 my-2">
                             <label htmlFor="name" className="font-base text-sm text-[#14213D] dark:text-[#E5E5E5] mb-1">Name</label>
-                            <input type="text" placeholder="ex : Jhon Doe" required 
+                            <input type="text" placeholder="Your Name" required 
                             id="name"
                             name="name" 
                             className="text-[#14213D] border border-[#14213D] outline-none px-3 py-2 rounded-md w-full dark:bg-[#14213D] dark:border-[#E5E5E5] dark:text-[#E5E5E5]" />
                         </div>
                         <div className="w-full block gap-2 my-2">
                             <label htmlFor="object" className="font-base text-sm text-[#14213D] dark:text-[#E5E5E5] mb-1">Object</label>
-                            <input type="text" placeholder="ex : i want to work with you" required
+                            <input type="text" placeholder="What's your object" required
                             id="subject"
                             name="subject"
                             className="text-[#14213D] border border-[#14213D] outline-none px-3 py-2 rounded-md w-full dark:bg-[#14213D] dark:border-[#E5E5E5] dark:text-[#E5E5E5]" />
                         </div>
                         <div className="w-full block gap-2 my-2">
                             <label htmlFor="email" className="font-base text-sm text-[#14213D] dark:text-[#E5E5E5] mb-1">Email</label>
-                            <input type="email" placeholder="ex : jhon@example.com" required
+                            <input type="email" placeholder="your.email@example.com" required
                             id="email"
                             name="email"
                             className="text-[#14213D] border border-[#14213D] outline-none px-3 py-2 rounded-md w-full dark:bg-[#14213D] dark:border-[#E5E5E5] dark:text-[#E5E5E5]" />
                         </div>
                         <div className="w-full block gap-2 my-2">
                             <label htmlFor="message" className="font-base text-sm text-[#14213D] dark:text-[#E5E5E5] mb-1">Message</label>
-                            <textarea rows="5" placeholder="ex : Hello..." required
+                            <textarea rows="5" placeholder="How can we help you ?" required
                                 id="message"
                                 name="message"
                                 className="text-[#14213D] border border-[#14213D] outline-none px-3 py-2 rounded-md w-full dark:bg-[#14213D] dark:border-[#E5E5E5] dark:text-[#E5E5E5]"
