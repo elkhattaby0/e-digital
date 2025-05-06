9ca757767a8d16b252bd5b3991fe2273549f41be
elkhattaby0 <eelkhattaby@gmail.com>
2025-05-06 18:40:57 +0100

    v1 changing motion

--- src/Layouts/Homepage/Hero.js
+++ src/Layouts/Homepage/Hero.js
@@ -8,33 +8,6 @@ const Hero = () => {
     return (
         <section className={Style.container} >
             <div className={Style.section} >
-                <div className={Style.mobile}>
-                    <motion.h1 className={Style.h1}
-                        initial={{ opacity: 0, y: -100 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 0.9, ease: "easeOut" }}
-                    >
-                        <span className={Style.word}>Smart</span> Web & App Solutions
-                    </motion.h1>
-                    <motion.h2 className={Style.h2} 
-                        initial={{ opacity: 0, y: -100 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 1, ease: "easeOut" }}
-                    >
-                        Build Online. Grow Fast. 
-                    </motion.h2>
-                    <Paragraph isCenter={true}>
-                        We craft modern website and mobile apps to help your business stand out
-                    </Paragraph>
-                    <motion.a href="/contact" className={Style.a} 
-                        initial={{ opacity: 0, y: -100 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 1.3, ease: "easeOut" }}
-                    >
-                        Get a Free Quote
-                        <TrendingUp size="20px" className="ml-1" />
-                    </motion.a>
-                </div>
                 <div className={Style.left}>
                     <motion.h1 className={Style.h1}
                         initial={{ opacity: 0, x: -100 }}
@@ -56,7 +29,7 @@ const Hero = () => {
                     <motion.a href="/contact" className={Style.a} 
                         initial={{ opacity: 0, x: -100 }}
                         animate={{ opacity: 1, x: 0 }}
-                        transition={{ duration: 1.5, ease: "easeOut" }}
+                        transition={{ duration: 1.8, ease: "easeOut" }}
                     >
                         Get a Free Quote
                         <TrendingUp size="20px" className="ml-1" />
@@ -76,12 +49,11 @@ const Hero = () => {
 
 const Style = {
     "container": "w-full flex items-center justify-center max-md:mt-6",
-    "section": "w-11/12 flex max-md:flex-col items-center justify-center h-[calc(100lvh-80px)]",
-    "left": "w-2/3 max-md:w-full bor der max-md:hidden",
-    "mobile": "hidden max-md:flex flex-col justify-center items-center w-full",
+    "section": "w-11/12 flex max-md:flex-col items-center justify-center h-[calc(100lvh-80px)] max-md:h-full",
+    "left": "w-2/3 max-md:w-full bor der " ,
     "right": "w-1/3 max-md:w-full bo rder",
-    "h1": "text-6xl font-bold text-[#14213D] mb-4 dark:text-[#E5E5E5] max-md:text-center",
-    "h2": "text-3xl font-base text-[#14213D] leading-[38px]  mb-4 dark:text-[#E5E5E5] max-md:text-center",
+    "h1": "text-6xl font-bold text-[#14213D] mb-4 dark:text-[#E5E5E5]",
+    "h2": "text-3xl font-base text-[#14213D] leading-[38px]  mb-4 dark:text-[#E5E5E5]",
     "a": "flex items-center bg-[#14213D] text-[#FFFFFF] rounded-full px-4 py-2 dark:bg-[#E5E5E5] dark:text-[#14213D] outline-none cursor-pointer transition-all duration-300 ease-in-out hover:bg-[#000000] dark:hover:bg-[#14213D] dark:hover:text-[#E5E5E5] border-[1px] dark:hover:border-[#E5E5E5] w-fit",
     "word": "text-[#FCA311]"
 }
